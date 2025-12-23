21669d2d222f833056b572e20f35d6569d0cc4d1
YgorPereira <ygorrp25@gmail.com>
2025-12-23 19:41:12 -0300

    be-fix: fix wrong file names

--- apps/backend/src/modules/schedule/presentation/schedule.controller.spec.ts
+++ apps/backend/src/modules/schedule/presentation/schedule.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ShedulingController } from './scheduling.controller';
+import { ShedulingController } from './schedule.controller';
 
 describe('ShedulingController', () => {
   let controller: ShedulingController;
